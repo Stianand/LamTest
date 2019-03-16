7451369152dc4da5e3dbacb46c47ad3dc876cef8
Stian Andreassen <stianandreassen@Stians-MacBook-Pro.local>
2019-03-16 07:58:43 +0100

    test3

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var server = ParseServer({
   // The public URL of your app.
   // This will appear in the link that is used to verify email addresses and reset passwords.
   // Set the mount path as it is in serverURL
-  publicServerURL: 'http://lamworkappserver.herokuapp.com/parse',
+  publicServerURL: 'http://lamworkdemo.herokuapp.com/parse',
   // Your apps name. This will appear in the subject and body of the emails that are sent.
-  appName: 'lamworkapplication',
+  appName: 'lamworkdemo',
   // The email adapter
   emailAdapter: {
     module: 'parse-server-simple-mailgun-adapter',
